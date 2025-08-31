87b78cf53e1cc58eeb6073967099560d983f8c9f
Gopikrishna Mandapati <mandapati7@users.noreply.github.com>
2025-08-31 14:06:58 -0400

    feat(client): simplify App component and remove unused styles

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -8,6 +8,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.send({"message": "Hello, World!"});
+});
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
